49ed35fb1ca3844714e4aa5002a3ca8bd69f5017
Parli Rizal <74726025+parlii@users.noreply.github.com>
2023-10-04 11:23:48 -0700

    pass credentials to vertex ai

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         credentials=credentials
     )
 
-    st.write(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+    # st.write(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 
     # query = "How to create a new service?"
 
@@ -94,16 +94,17 @@ if __name__ == "__main__":
     #     st.write(doc.metadata['source'])
     #     st.write(doc.page_content)
 
-    # parameters = {
-    #     "temperature": 0,
-    #     "max_output_tokens": 2000,
-    #     "model_name": "text-bison@latest"
-    # }
+    parameters = {
+        "temperature": 0,
+        "max_output_tokens": 2000,
+        "model_name": "text-bison@latest",
+        "credentials": credentials
+    }
 
-    # chain = RetrievalQAWithSourcesChain.from_chain_type(
-    #     VertexAI(**parameters), chain_type="stuff", retriever=retriever, return_source_documents=True)
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        VertexAI(**parameters), chain_type="stuff", retriever=retriever, return_source_documents=True)
 
-    # result = chain({"question": "How to create a new service?"})
-    # st.write(result)
+    result = chain({"question": "How to create a new service?"})
+    st.write(result)
 
     main()
